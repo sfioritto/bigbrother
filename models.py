5151640effeb4cf4994a8805dcbec2da4b103a98
Sean Fioritto <sean.fioritto@gmail.com>
2011-12-04 22:37:33 -0600

    roughins for db models.

---
+++ models.py
@@ -0,0 +1,21 @@
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Identity(Base):
+    __tablename__ = 'identities'
+    id = Column(Integer, primary_key=True)
+
+
+class Whorl(Base):
+    __tablename__ = 'whorls'
+    key = Column(String(128), primary_key=True)
+    count = Column(Integer)
+    
+class WhorlGivenIdentity(Base):
+    __tablename__ = 'whorl_given_identity'
+    whorl_key = Column(String(128), ForeignKey('whorls.key'), primary_key=True)
+    identity_id = Column(Integer, ForeignKey('identities.id'), primary_key=True)
+    count = Column(Integer)
+
+
