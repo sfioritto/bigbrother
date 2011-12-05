4d2dd35cda21652c3110572eb5d3e17347a165fe
Sean Fioritto <sean.fioritto@gmail.com>
2011-12-05 12:16:34 -0600

    basic logic for creating whorls implemented.

--- app.py
+++ app.py
@@ -2,25 +2,63 @@ import os
 import tornado.ioloop
 import tornado.web
 import json
+from models import Whorl, engine, Session
+from sqlalchemy.orm import scoped_session
+from hashlib import sha512
+from sqlalchemy.orm.exc import NoResultFound
 
 
-class Identify(tornado.web.RequestHandler):
+
+class Application(tornado.web.Application):
+
+    def __init__(self):
+
+        self.session = scoped_session(Session)
+
+        settings = {
+            "static_path": os.path.join(os.path.dirname(__file__), "static")
+            }
+
+
+        handlers = [
+            (r"/identify", Identify),
+            (r"/(tester\.html)", tornado.web.StaticFileHandler,
+             dict(path=settings['static_path']))]
+
+
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+
+class BaseHandler(tornado.web.RequestHandler):
+
+    @property
+    def session(self):
+        return self.application.session
+
+
+
+class Identify(BaseHandler):
+
     def post(self):
         whorls = json.loads(self.request.body)
-        
-        self.write("")
+        for key, value in whorls.items():
+            hashed = sha512(key + str(value)).hexdigest()
+
+            try:
+                whorl = self.session.query(Whorl).filter_by(key=hashed).one()
+                whorl.count = whorl.count + 1
+                self.write("found old value")
+
+            except NoResultFound:
+                whorl = Whorl(key=hashed)
+                self.session.add(whorl)
+                self.write("new value")
+
+        self.session.commit()
 
 
-settings = {
-    "static_path": os.path.join(os.path.dirname(__file__), "static")
-}
-
-application = tornado.web.Application([
-    (r"/identify", Identify),
-    (r"/(tester\.html)", tornado.web.StaticFileHandler,
-     dict(path=settings['static_path'])),
-], **settings)
 
 if __name__ == "__main__":
+    application = Application()
     application.listen(8080)
     tornado.ioloop.IOLoop.instance().start()
--- models.py
+++ models.py
@@ -1,7 +1,14 @@
-from sqlalchemy.ext.declarative import *
+import config
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Column, ForeignKey, Integer, String
+
+
 Base = declarative_base()
 
+engine = create_engine(config.dbconnection)
+Session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 class Identity(Base):
     __tablename__ = 'identities'
@@ -11,13 +18,13 @@ class Identity(Base):
 class Whorl(Base):
     __tablename__ = 'whorls'
     key = Column(String(128), primary_key=True)
-    count = Column(Integer)
+    count = Column(Integer, default=1)
 
     
 class WhorlGivenIdentity(Base):
     __tablename__ = 'whorl_given_identity'
     whorl_key = Column(String(128), ForeignKey('whorls.key'), primary_key=True)
     identity_id = Column(Integer, ForeignKey('identities.id'), primary_key=True)
-    count = Column(Integer)
+    count = Column(Integer, default=0)
 
 
