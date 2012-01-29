9ba1201d8d3547559c5b7812ff3349fc0025a655
= <=>
2012-01-29 11:17:24 -0600

    code cleanup. few bug fixes.

--- static/lift.js
+++ static/lift.js
@@ -8,28 +8,15 @@
 		      version: plugin.version}
     },
 
-    whorls = {};
+    whorls = {},
     
-    whorls.plugins = _.map(navigator.plugins, parsePlugin);
-    whorls.timezone = (new Date()).getTimezoneOffset();
-    whorls.screen = screen;
-    whorls.cookiesenabled = navigator.cookieEnabled;
-    whorls.localstorage = !!localStorage;
-    whorls.sessionstorage = !!sessionStorage;
-
-    if (navigator.userAgent === "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:7.0.1) Gecko/20100101 Firefox/7.0.1"){
-	whorls.username = "firefox"
-    } else {
-	whorls.username = "chrome"
-    }
-
-    var ec = new evercookie(),
+    ec = new evercookie(),
 
     get_history = function(){
 	visipisi.get(function(results){
 	    whorls.history = results;
-	    console.log(whorls);
 	    identify(whorls, function(response){
+		console.log(whorls);
 		console.log(response);
 	    });
 	});
@@ -39,6 +26,7 @@
 	
 	// Check Flash version
 	if (!swfobject.hasFlashPlayerVersion("9.0.0")){
+	    //just skip fonts.
 	    cb();
 	} else {
             var fontDetect = new FontDetect("fontdetectswf", "FontList.swf", function(fd) {        
@@ -48,20 +36,9 @@
 	    });
 
 	}	
-    };
-    
-    $(document).ready(get_fonts);
-    
-    ec.get("uid", function(best, all) {
-	whorls.evercookie = all;
-	whorls.username = "Sean";
-	whorls.password = "password";
-	get_fonts(get_history);
-    });
-    
-    //    ec.set("uid", "112")
-    
-    var learn = function(whorls, callback){
+    },
+
+    learn = function(whorls, callback){
 	$.ajax({url: "/learn", 
 		processData: false,
 		type: "POST",
@@ -79,5 +56,27 @@
 	});
     };
 
+    
+    $(document).ready(function(){
+	whorls.plugins = _.map(navigator.plugins, parsePlugin);
+	whorls.timezone = (new Date()).getTimezoneOffset();
+	whorls.screen = screen;
+	whorls.cookiesenabled = navigator.cookieEnabled;
+	whorls.localstorage = !!localStorage;
+	whorls.sessionstorage = !!sessionStorage;
+	
+	if (navigator.userAgent === "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:7.0.1) Gecko/20100101 Firefox/7.0.1"){
+	    whorls.username = "firefox"
+	} else {
+	    whorls.username = "chrome"
+	}
+	
+	ec.get("uid", function(best, all) {
+	    whorls.evercookie = all;
+	    whorls.username = "Sean";
+	    whorls.password = "password";
+	    get_fonts(get_history);
+	});
+    });
 
 })();
