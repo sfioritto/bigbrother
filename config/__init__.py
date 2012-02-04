54c9367b3a5e1e88b5e8768da5940bdfdcb9d6e6
= <=>
2012-02-04 13:15:47 -0600

    recovered?

--- config/__init__.py
+++ config/__init__.py
@@ -3,8 +3,8 @@ import json, os
 # later you can detect what environment you are in, somehow, maybe
 # a command line flag or something
 
-
-for key, value in json.loads(open("config/dev.json").read()).items():
+path = os.path.dirname(__file__)
+for key, value in json.loads(open(os.path.join(path, "dev.json")).read()).items():
     vars()[key] = value
 
 
---
+++ fabfile.py
@@ -0,0 +1,91 @@
+from fabric.api import *
+
+env.hosts = ['nymbus.org']
+env.approot = "/var/nymbus/www"
+env.prodhome = "/var/nymbus"
+
+
+def test():
+    # run tests here if you got 'em
+    # local("nosetests tests/")
+    pass
+
+
+def pack(hash):
+
+    """
+    Creates a clean copy of the code
+    """
+    archivename = "%s.tar.gz" % hash
+    local("git archive --format=tar %s | gzip > /tmp/%s;" % (hash, archivename))
+    return archivename
+
+
+def prepare(hash):
+
+    """
+    Test and archive postosaurus.
+    """
+
+    test()
+    pack(hash)
+
+
+def upload(archive):
+    put('/tmp/%s' % archive, '/tmp/')
+
+
+def untar(archive, hash):
+    with settings(warn_only=True):
+        with cd(env.prodhome):
+            sudo("rm -rf snapshots/%s" % hash)
+            sudo("mkdir snapshots/%s" % hash)
+            sudo("cd snapshots/%s; tar -xvf '/tmp/%s'" % (hash,archive))
+
+
+def upload_untar(archive, hash):
+    upload(archive)
+    untar(archive, hash)
+
+
+def switch(hash):
+    with cd(env.prodhome):
+        sudo("ln -s %s/snapshots/%s/webapp /tmp/live_tmp && sudo mv -Tf /tmp/live_tmp %s" % (env.prodhome, hash, env.approot))
+
+    with cd(env.approot):
+        # copy production settings files here if applicable
+        pass
+
+
+def stop():
+    #TODO: this will break when there are multiple hosts, need to dynamically lookup uid and gid
+    #This assert makes sure I know when it breaks
+    assert len(env.hosts) == 1, "This script only works with one host."
+
+    with settings(warn_only=True):
+        sudo("/etc/init.d/nginx stop")
+        #todo: stop fcgi processes
+
+
+def start():
+    #TODO: this will break when there are multiple hosts, need to dynamically lookup uid and gid
+    #This assert makes sure I know when it breaks
+    assert len(env.hosts) == 1, "This script only works with one host"
+
+    with settings(warn_only=True):
+        sudo("/etc/init.d/nginx stop")
+#        spawn-fcgi -d /path/to/www -f /path/to/www/index.py -a 127.0.0.1 -p 9002 -F 5
+        #todo: start fcgi processes
+
+def reboot():
+    stop()
+    start()
+
+    
+def deploy(hash):
+    
+    test()
+    archive = pack(hash)
+    upload(archive)
+    untar(archive, hash)
+    switch(hash)
